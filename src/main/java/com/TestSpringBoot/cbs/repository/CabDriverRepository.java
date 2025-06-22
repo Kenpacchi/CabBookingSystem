8d55bb13dff33d001112ebf8f27bd86526d59f1b
Ajay <ajaygound.bhu@gmail.com>
2025-06-22 18:56:17 +0530

    minor changes

--- src/main/java/com/TestSpringBoot/cbs/repository/CabDriverRepository.java
+++ src/main/java/com/TestSpringBoot/cbs/repository/CabDriverRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CabDriverRepository extends JpaRepository<CabDriver, Integer> {
+public interface CabDriverRepository extends JpaRepository<CabDriver, Long> {
 }
